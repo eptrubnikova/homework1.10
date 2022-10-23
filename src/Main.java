8e07dbca40355c765f6b18e42c3e6eaee06e8da6
eptrubnikova <eptrubnikova@gmail.com>
2022-10-23 15:58:08 +0600

    task 3

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //      String phone = "+7(999)-79 4516!";
-        //    phone = phone.replace("-", "");
-        //  phone = phone.replace(" ", "");
-        // phone = phone.replace("(", "");
-        // phone = phone.replace(")", "");
-        // phone = phone.replace("+", "");
-        // if (phone.length() == 10) {
-        //     phone = '7' + phone;
-        // } else if (phone.length() > 11) {
-        //    throw new RuntimeException("Телефон слишком длинный");
-        // } else if (phone.length() < 10) {
-        //    throw new RuntimeException("Телефон слишком короткий");
-        // } else if (phone.length() == 11 && phone.charAt(0) != '7') {
-        //    throw new RuntimeException("Среди нас посторонний");
-        //}
-        // System.out.println("phone = " + phone);
-        // String expectedPhone = "79994537916";
-        // if (phone.equals(expectedPhone)) {
-        //    System.out.println("Успех!");
-        // } else {
-        //    System.out.println("Неудача!");
-        // }
+
 
         task1();
         task2();
