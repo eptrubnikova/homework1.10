c84b70d964468feceebf4ddc321eb811b41b8a11
eptrubnikova <eptrubnikova@gmail.com>
2022-10-23 15:49:59 +0600

    task 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        //      String phone = "+7(999)-79 4516!";
+        //    phone = phone.replace("-", "");
+        //  phone = phone.replace(" ", "");
+        // phone = phone.replace("(", "");
+        // phone = phone.replace(")", "");
+        // phone = phone.replace("+", "");
+        // if (phone.length() == 10) {
+        //     phone = '7' + phone;
+        // } else if (phone.length() > 11) {
+        //    throw new RuntimeException("Телефон слишком длинный");
+        // } else if (phone.length() < 10) {
+        //    throw new RuntimeException("Телефон слишком короткий");
+        // } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+        //    throw new RuntimeException("Среди нас посторонний");
+        //}
+        // System.out.println("phone = " + phone);
+        // String expectedPhone = "79994537916";
+        // if (phone.equals(expectedPhone)) {
+        //    System.out.println("Успех!");
+        // } else {
+        //    System.out.println("Неудача!");
+        // }
 
+        task1();
+
+    }
+
+    private static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        String fuiiNameForReport = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fuiiNameForReport);
     }
 }
